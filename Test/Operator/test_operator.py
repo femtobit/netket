a829fc3c87a5c5538ddce4d84d74dcac8453a079
Damian Hofmann <damian.hofmann@mpsd.mpg.de>
2019-06-09 19:36:26 -0400

    Fix tests on Python 2

--- Test/Operator/test_operator.py
+++ Test/Operator/test_operator.py
@@ -133,7 +133,7 @@ def test_operator_is_hermitean():
 
 def test_no_segfault():
     g = nk.graph.Hypercube(8, 1)
-    hi = nk.hilbert.Spin(g, 1/2)
+    hi = nk.hilbert.Spin(g, 0.5)
 
     lo = nk.operator.LocalOperator(hi, [[1,0],[0,1]], [0])
     lo = lo.transpose()
